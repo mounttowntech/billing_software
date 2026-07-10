dc5bae0dedfd16ff12731ae5c34903bef2d81efb
mounttowntech/Vinitha <mounttowntech@gmail.com>
2026-07-10 15:57:48 +0530

    update and create code commit

--- src/routes/stockledgerRoutes.js
+++ src/routes/stockledgerRoutes.js
@@ -2,15 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 const {
+createStockLedger,
 getStockLedgers,
 getStockLedgerById,
+updateStockLedgerById,
 deleteStockLedger
 } = require(
 "../controllers/stockledgerController"
 );
+router.post(
+  "/create",
+createStockLedger
+);
 
 router.get("/all",getStockLedgers);
 router.get("/:id",getStockLedgerById);
+router.put(
+  "/update/:id",
+  updateStockLedgerById
+);
 router.delete("/delete/:id",deleteStockLedger);
 
 module.exports = router;
