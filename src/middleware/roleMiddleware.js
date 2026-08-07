324ec9f2646b95a03b34750b70842e3868cf8b09
vikash-mounttowntech <mounttowntech@gmail.com>
2026-08-07 12:16:15 +0530

    allowRoles added

--- src/middleware/roleMiddleware.js
+++ src/middleware/roleMiddleware.js
@@ -1,6 +1,7 @@
 exports.allowRoles = (...roles) => {
   return (req, res, next) => {
-    if (!req.user || !roles.includes(req.user.role)) {
+    console.log("User role:", req.user.role?.roleName);
+    if (!req.user || !roles.includes(req.user.role?.roleName?.toLowerCase())) {
       return res.status(403).json({
         success: false,
         message: "Permission denied",
